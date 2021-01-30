19e8295bcfeaf28cc50625625947f791399fcdbc
The Noise <leespaulding25@gmail.com>
2021-01-30 09:37:09 -0500

    created input area for HTML and Varibles for Js Script

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+var button = document.querySelector('.button')
+var inputValue =document.querySelector('.inputValue')
+var name = document.querySelector('.name');
+var desc = document.querySelector('desc');
+var temp =document.querySelector('.temp');
 
