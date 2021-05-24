b4b93e8a307e90aa3b4038ed34315519c846ab0c
soulfulnoise <leespaulding25@gmail.com>
2021-05-24 08:55:36 -0400

    added ids to search form and changed submit to to search

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const { $CombinedState } = require("redux");
-
 //varibles
 let searchForm = document.getElementById("city-search");
 let searchButton = document.getElementById("search-button");
@@ -10,7 +8,7 @@ let ApiKey ="f942c0f8f979e4f33b7d3cd78a52416d"
 
 function currentWeather(name) {
     let city =name;
-    let queryURL = "api.openweathermap.org/data/2.5/weather?q=" 
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" 
     + name +"&units=imperial" +"&appid=" + ApiKey;
 fetch(queryURL)
     .then((response) => {
@@ -20,14 +18,14 @@ fetch(queryURL)
     console.log(result);
     forecastWeather(name);
     saveInput(city);
-    let icon ="https://openweathermap.org/img/w/" = result.weather[0].icon + "png";
+    let icon ="https://openweathermap.org/img/w/" === result.weather[0].icon + "png";
     renderLastSearch();
 
     let currentTimeExt = result.dt;
     let currentTime = moment.unix(currentTimeExt);
     console.log(currentTime);
 
-    let todaysWeatherHtml =`
+    let currentWeatherHtml =`
     <div>
     <div class="col" id="city-name">
     <span id="place" class="align-middle"> ${result.name} ${currentTime.format("(MM/DD/YY HH:mm:ss)")}
@@ -45,11 +43,21 @@ fetch(queryURL)
     </div>
     </div>`;
    
-    $(todaysWeatherInfo).html(todaysWeatherHtml);
+    $(currentWeatherInfo).html(currentWeatherHtml);
 })
 .catch(error => {
     console.log(error);
 });
+
 };
+
+function forecastWether(name) {
+    let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" 
+    + name +"&units=imperial" +"&appid=" + ApiKey;
+fetch(queryURL)
+    .then((response) => {
+        return response.json()
+    })
+}
 
 
