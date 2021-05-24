4dc8e12962019da2e2096cb88d8dec81d9f2d181
soulfulnoise <leespaulding25@gmail.com>
2021-05-24 07:57:10 -0400

    set up past city searchs

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ var name = document.querySelector('.name');
 var desc = document.querySelector('desc');
 var temp =document.querySelector('.temp');
 
+
+
+button.addEventListener("click",function(){
+   fetch('api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid={f942c0f8f979e4f33b7d3cd78a52416d}')
+    .then(response => response.json())
+    .then(data => console.log(data))
+
+    .catch(err => alert("Wrong city name!"))
+})
