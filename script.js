f350765e71bed859520edf9804d77f881c44bc3c
soulfulnoise <leespaulding25@gmail.com>
2021-05-24 08:17:52 -0400

    added api and api ket saved city info to localstorage and add icons

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-var button = document.querySelector('.button')
-var inputValue =document.querySelector('.inputValue')
-var name = document.querySelector('.name');
-var desc = document.querySelector('desc');
-var temp =document.querySelector('.temp');
+//varibles
+let searchForm = document.getElementById("city-search");
+let searchButton = document.getElementById("search-button");
 
 
 
-button.addEventListener("click",function(){
-   fetch('api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid={f942c0f8f979e4f33b7d3cd78a52416d}')
-    .then(response => response.json())
-    .then(data => console.log(data))
+let ApiKey ="f942c0f8f979e4f33b7d3cd78a52416d"
 
-    .catch(err => alert("Wrong city name!"))
+function currentWeather(name) {
+    let city =name;
+    let queryURL = "api.openweathermap.org/data/2.5/weather?q=" 
+    + name +"&units=imperial" +"&appid=" + ApiKey;
+fetch(queryURL)
+    .then((response) => {
+        return response.json()
+    })
+.then(result => {
+    console.log(result);
+    forecastWeather(name);
+    saveInput(city);
+    let icon ="https://openweathermap.org/img/w/" = result.weather[0].icon + "png";
+    renderLastSearch();
 })
+}
+
+
